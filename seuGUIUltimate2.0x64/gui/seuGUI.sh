7ff93bd6835dd1bdf9fd841ba4c95c228ac8f610
zjh <zjhsdtc@gmail.com>
2013-09-06 13:22:09 +0800

    cleantha

---
+++ seuGUIUltimate2.0x64/gui/seuGUI.sh
@@ -0,0 +1,4 @@
+export CLASSPATH=.:/home/robocup3d/j3d/lib/ext/j3dcore.jar:/home/robocup3d/j3d/lib/ext/j3dutils.jar:/home/robocup3d/j3d/lib/ext/vecmath.jar
+export LD_LIBRARY_PATH=/home/robocup3d/j3d/lib/amd64
+#!cd gui/
+java -jar seuGUIUltimate2.0
---
+++ seuGUIUltimate2.0x86/gui/seuGUI.sh
@@ -0,0 +1,4 @@
+export CLASSPATH=.:/home/zjhclea/j3d/lib/ext/j3dcore.jar:/home/zjhclea/j3d/lib/ext/j3dutils.jar:/home/zjhclea/j3d/lib/ext/vecmath.jar
+export LD_LIBRARY_PATH=/home/zjhclea/j3d/lib/i386
+#!cd gui/
+java -jar seuGUIUltimate2.0
